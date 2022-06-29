4f3154ddc64a937ab374a1aa37547e15d68cecf1
mlbase <guddnr2424@gmail.com>
2022-06-29 23:47:10 +0900

    hibernate ddl

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 public class Member {
@@ -9,6 +10,7 @@ public class Member {
     @Id
     private Long id;
     private String name;
+    private int age;
 
     public Member() {
     }
@@ -32,5 +34,13 @@ public class Member {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
